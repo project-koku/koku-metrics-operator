8d861756be26a7dec6c0c5ca350a736b6c5b0cf8
Michael Skarbek <mskarbek@redhat.com>
2020-11-19 18:05:55 -0500

    inital test

--- sources/handler_test.go
+++ sources/handler_test.go
@@ -19,17 +19,82 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package sources
 
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	logr "github.com/go-logr/logr/testing"
+	"github.com/project-koku/korekuta-operator-go/crhchttp"
+)
+
+var (
+	// cost       = &crhchttp.CostManagementConfig{Log: zap.New()}
+	errSources = errors.New("test error")
+	// log        logr.TestLogger
+)
+
 // https://www.thegreatcodeadventure.com/mocking-http-requests-in-golang/
-// type MockClient struct {
-// 	DoFunc func(req *http.Request) (*http.Response, error)
-// }
+type MockClient struct {
+	res *http.Response
+	err error
+}
 
-// var (
-// 	// GetDoFunc fetches the mock client's `Do` func
-// 	GetDoFunc func(req *http.Request) (*http.Response, error)
-// )
+// Do is the mock client's `Do` func
+func (m MockClient) Do(req *http.Request) (*http.Response, error) {
+	return m.res, m.err
+}
 
-// // Do is the mock client's `Do` func
-// func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
-// 	return GetDoFunc(req)
-// }
+func TestGetSourceTypeID(t *testing.T) {
+	log := logr.TestLogger{T: t}
+	cost := &crhchttp.CostManagementConfig{Log: log}
+
+	getSourceTypeIDTests := []struct {
+		name        string
+		response    *http.Response
+		responseErr error
+		expected    string
+		expectedErr error
+	}{
+		{
+			name: "successful response with data",
+			response: &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(strings.NewReader("{\"meta\":{\"count\":1},\"data\":[{\"id\":\"1\",\"name\":\"openshift\"}]}")), // type is io.ReadCloser,
+				Request:    &http.Request{Method: "GET", URL: &url.URL{}},
+			},
+			responseErr: nil,
+			expected:    "1",
+			expectedErr: nil,
+		},
+		{
+			name: "400 bad response - malformed url",
+			response: &http.Response{
+				StatusCode: 400,
+				Body:       ioutil.NopCloser(strings.NewReader("{\"errors\":[{\"status\":\"400\",\"detail\":\"ArgumentError: Failed to find definition for Name\"}]}")), // type is io.ReadCloser,
+				Request:    &http.Request{Method: "GET", URL: &url.URL{}},
+			},
+			responseErr: nil,
+			expected:    "",
+			expectedErr: errSources,
+		},
+	}
+	for _, tt := range getSourceTypeIDTests {
+		t.Run(tt.name, func(t *testing.T) {
+			clt := MockClient{res: tt.response, err: tt.responseErr}
+			got, err := GetSourceTypeID(cost, clt)
+			if tt.expectedErr != nil && err == nil {
+				t.Errorf("%s expected error, got: %v", tt.name, err)
+			}
+			if tt.expectedErr == nil && err != nil {
+				t.Errorf("%s got unexpected error: %v", tt.name, err)
+			}
+			if got != tt.expected {
+				t.Errorf("%s got %s want %s", tt.name, got, tt.expected)
+			}
+		})
+	}
+}
