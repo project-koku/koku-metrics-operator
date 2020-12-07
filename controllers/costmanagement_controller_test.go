eb10d7acd6931672e12216b454861f4583fa835a
abaiken <aaiken@redhat.com>
2020-12-07 15:42:45 -0500

    tmp commit so I can merge

--- controllers/costmanagement_controller_test.go
+++ controllers/costmanagement_controller_test.go
@@ -21,7 +21,10 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -50,6 +53,87 @@ var defaultCreateSource bool = false
 var defaultSkipTLSVerify bool = true
 var defaultValidateCert bool = true
 
+func Copy(mode os.FileMode, src, dst string) (os.FileInfo, error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return nil, err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return nil, err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return nil, err
+	}
+	info, err := out.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.Chmod(out.Name(), mode); err != nil {
+		return nil, err
+	}
+
+	return info, out.Close()
+}
+
+func setup() error {
+	type dirInfo struct {
+		dirName  string
+		files    []string
+		dirMode  os.FileMode
+		fileMode os.FileMode
+	}
+	testFiles := []string{"ocp_node_label.csv", "nonCSV.txt", "testFile.tar.gz"}
+	dirInfoList := []dirInfo{
+		{
+			dirName:  "data",
+			files:    testFiles,
+			dirMode:  0777,
+			fileMode: 0777,
+		},
+		{
+			dirName:  "upload",
+			files:    testFiles,
+			dirMode:  0777,
+			fileMode: 0777,
+		},
+	}
+	// setup the initial testing directory
+	fmt.Println("Setting up for packaging tests")
+	testingDir := "/tmp/cost-mgmt-operator-reports/"
+	if _, err := os.Stat(testingDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(testingDir, os.ModePerm); err != nil {
+			return fmt.Errorf("could not create %s directory: %v", testingDir, err)
+		}
+	}
+	for _, directory := range dirInfoList {
+		reportPath := filepath.Join(testingDir, directory.dirName)
+		if _, err := os.Stat(reportPath); os.IsNotExist(err) {
+			if err := os.Mkdir(reportPath, directory.dirMode); err != nil {
+				return fmt.Errorf("could not create %s directory: %v", reportPath, err)
+			}
+			for _, reportFile := range directory.files {
+				_, err := Copy(directory.fileMode, filepath.Join("../packaging/test_files/", reportFile), filepath.Join(reportPath, reportFile))
+				if err != nil {
+					return fmt.Errorf("could not copy %s file: %v", reportFile, err)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func shutdown() {
+	fmt.Println("Tearing down for packaging tests")
+	os.RemoveAll("/tmp/cost-mgmt-operator-reports/")
+}
+
 var _ = Describe("CostmanagementController", func() {
 
 	const timeout = time.Second * 60
@@ -58,10 +142,11 @@ var _ = Describe("CostmanagementController", func() {
 
 	BeforeEach(func() {
 		// failed test runs that don't clean up leave resources behind.
-		os.Mkdir("foo", 0777)
+		shutdown()
 	})
 
 	AfterEach(func() {
+		// shutdown()
 
 	})
 
@@ -623,5 +708,171 @@ var _ = Describe("CostmanagementController", func() {
 			// Expect(fetched.Status.APIURL).To(Equal(costmgmtv1alpha1.DefaultAPIURL))
 			// Expect(fetched.Status.ClusterID).To(Equal(clusterID))
 		})
+		It("should run through more of the flow", func() {
+			setup()
+			createClusterVersion(clusterID)
+			var uploadWait int64 = 0
+			instance := costmgmtv1alpha1.CostManagement{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      namePrefix + "more",
+					Namespace: namespace,
+				},
+				Spec: costmgmtv1alpha1.CostManagementSpec{
+					Authentication: costmgmtv1alpha1.AuthenticationSpec{
+						AuthType: costmgmtv1alpha1.Token,
+					},
+					Packaging: costmgmtv1alpha1.PackagingSpec{
+						MaxSize: 100,
+					},
+					Upload: costmgmtv1alpha1.UploadSpec{
+						UploadCycle:    &defaultUploadCycle,
+						UploadToggle:   &defaultUploadToggle,
+						UploadWait:     &uploadWait,
+						IngressAPIPath: "/api/ingress/v1/upload",
+						ValidateCert:   &defaultValidateCert,
+					},
+					Source: costmgmtv1alpha1.CloudDotRedHatSourceSpec{
+						CreateSource:   &defaultCreateSource,
+						SourcesAPIPath: "/api/sources/v1.0/",
+						CheckCycle:     &defaultCheckCycle,
+					},
+					PrometheusConfig: costmgmtv1alpha1.PrometheusSpec{
+						SkipTLSVerification: &defaultSkipTLSVerify,
+						SvcAddress:          "https://thanos-querier.openshift-monitoring.svc:9091",
+					},
+					APIURL: "https://cloud.redhat.com",
+				},
+			}
+
+			Expect(k8sClient.Create(ctx, &instance)).Should(Succeed())
+			time.Sleep(time.Second * 10)
+
+			fetched := &costmgmtv1alpha1.CostManagement{}
+
+			// check the CRD was created ok
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: namespace}, fetched)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			// Expect(fetched.Status.Authentication.AuthType).To(Equal(costmgmtv1alpha1.DefaultAuthenticationType))
+			// Expect(*fetched.Status.Authentication.AuthenticationCredentialsFound).To(BeTrue())
+			// Expect(fetched.Status.APIURL).To(Equal(costmgmtv1alpha1.DefaultAPIURL))
+			// Expect(fetched.Status.ClusterID).To(Equal(clusterID))
+		})
+		It("should check the status", func() {
+			setup()
+			var uploadWait int64 = 0
+			uploadTime := metav1.Now()
+			instance := costmgmtv1alpha1.CostManagement{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      namePrefix + "status",
+					Namespace: namespace,
+				},
+				Spec: costmgmtv1alpha1.CostManagementSpec{
+					Authentication: costmgmtv1alpha1.AuthenticationSpec{
+						AuthType: costmgmtv1alpha1.Token,
+					},
+					Packaging: costmgmtv1alpha1.PackagingSpec{
+						MaxSize: 100,
+					},
+					Upload: costmgmtv1alpha1.UploadSpec{
+						UploadCycle:    &defaultUploadCycle,
+						UploadToggle:   &defaultUploadToggle,
+						UploadWait:     &uploadWait,
+						IngressAPIPath: "/api/ingress/v1/upload",
+						ValidateCert:   &defaultValidateCert,
+					},
+					Source: costmgmtv1alpha1.CloudDotRedHatSourceSpec{
+						CreateSource:   &defaultCreateSource,
+						SourcesAPIPath: "/api/sources/v1.0/",
+						CheckCycle:     &defaultCheckCycle,
+					},
+					PrometheusConfig: costmgmtv1alpha1.PrometheusSpec{
+						SkipTLSVerification: &defaultSkipTLSVerify,
+						SvcAddress:          "https://thanos-querier.openshift-monitoring.svc:9091",
+					},
+					APIURL: "https://cloud.redhat.com",
+				},
+				Status: costmgmtv1alpha1.CostManagementStatus{
+					Upload: costmgmtv1alpha1.UploadStatus{
+						LastSuccessfulUploadTime: uploadTime,
+					},
+				},
+			}
+
+			Expect(k8sClient.Create(ctx, &instance)).Should(Succeed())
+			time.Sleep(time.Second * 10)
+
+			fetched := &costmgmtv1alpha1.CostManagement{}
+
+			// check the CRD was created ok
+			Eventually(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: namespace}, fetched)
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
+
+			// Expect(fetched.Status.Authentication.AuthType).To(Equal(costmgmtv1alpha1.DefaultAuthenticationType))
+			// Expect(*fetched.Status.Authentication.AuthenticationCredentialsFound).To(BeTrue())
+			// Expect(fetched.Status.APIURL).To(Equal(costmgmtv1alpha1.DefaultAPIURL))
+			// Expect(fetched.Status.ClusterID).To(Equal(clusterID))
+		})
+		// It("should uploadagain", func() {
+		// 	setup()
+		// 	var uploadWait int64 = 0
+		// 	uploadTime := metav1.Now()
+		// 	instance := costmgmtv1alpha1.CostManagement{
+		// 		ObjectMeta: metav1.ObjectMeta{
+		// 			Name:      namePrefix + "uploadagain",
+		// 			Namespace: namespace,
+		// 		},
+		// 		Spec: costmgmtv1alpha1.CostManagementSpec{
+		// 			Authentication: costmgmtv1alpha1.AuthenticationSpec{
+		// 				AuthType: costmgmtv1alpha1.Token,
+		// 			},
+		// 			Packaging: costmgmtv1alpha1.PackagingSpec{
+		// 				MaxSize: 100,
+		// 			},
+		// 			Upload: costmgmtv1alpha1.UploadSpec{
+		// 				UploadCycle:    &uploadWait,
+		// 				UploadToggle:   &defaultUploadToggle,
+		// 				UploadWait:     &uploadWait,
+		// 				IngressAPIPath: "/api/ingress/v1/upload",
+		// 				ValidateCert:   &defaultValidateCert,
+		// 			},
+		// 			Source: costmgmtv1alpha1.CloudDotRedHatSourceSpec{
+		// 				CreateSource:   &defaultCreateSource,
+		// 				SourcesAPIPath: "/api/sources/v1.0/",
+		// 				CheckCycle:     &defaultCheckCycle,
+		// 			},
+		// 			PrometheusConfig: costmgmtv1alpha1.PrometheusSpec{
+		// 				SkipTLSVerification: &defaultSkipTLSVerify,
+		// 				SvcAddress:          "https://thanos-querier.openshift-monitoring.svc:9091",
+		// 			},
+		// 			APIURL: "https://cloud.redhat.com",
+		// 		},
+		// 		Status: costmgmtv1alpha1.CostManagementStatus{
+		// 			Upload: costmgmtv1alpha1.UploadStatus{
+		// 				LastSuccessfulUploadTime: uploadTime,
+		// 			},
+		// 		},
+		// 	}
+
+		// 	Expect(k8sClient.Create(ctx, &instance)).Should(Succeed())
+		// 	time.Sleep(time.Second * 10)
+
+		// 	fetched := &costmgmtv1alpha1.CostManagement{}
+
+		// 	// check the CRD was created ok
+		// 	Eventually(func() bool {
+		// 		err := k8sClient.Get(ctx, types.NamespacedName{Name: instance.Name, Namespace: namespace}, fetched)
+		// 		return err == nil
+		// 	}, timeout, interval).Should(BeTrue())
+
+		// 	// Expect(fetched.Status.Authentication.AuthType).To(Equal(costmgmtv1alpha1.DefaultAuthenticationType))
+		// 	// Expect(*fetched.Status.Authentication.AuthenticationCredentialsFound).To(BeTrue())
+		// 	// Expect(fetched.Status.APIURL).To(Equal(costmgmtv1alpha1.DefaultAPIURL))
+		// 	// Expect(fetched.Status.ClusterID).To(Equal(clusterID))
+		// })
 	})
 })
