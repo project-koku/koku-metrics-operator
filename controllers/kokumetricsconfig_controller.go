68a367dae658111383573c1480d7996297aa3a2a
Michael Skarbek <mskarbek@redhat.com>
2022-05-23 17:11:14 -0400

    use cfg.KokuMetricsConfig to update

--- controllers/kokumetricsconfig_controller.go
+++ controllers/kokumetricsconfig_controller.go
@@ -588,7 +588,7 @@ func configurePVC(r *MetricsConfigReconciler, req ctrl.Request, cfg *metricscfgv
 
 	if strings.Contains(cfg.Status.Storage.VolumeType, "EmptyDir") {
 		cfg.Status.Storage.VolumeMounted = false
-		if err := r.Status().Update(ctx, cfg); err != nil {
+		if err := r.Status().Update(ctx, cfg.KokuMetricsConfig); err != nil {
 			log.Error(err, "failed to update MetricsConfig status")
 		}
 		return &ctrl.Result{}, fmt.Errorf("PVC not mounted")
@@ -636,7 +636,7 @@ func (r *MetricsConfigReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	// set the cluster ID & return if there are errors
 	if err := setClusterID(r, cfg); err != nil {
 		log.Error(err, "failed to obtain clusterID")
-		if err := r.Status().Update(ctx, cfg); err != nil {
+		if err := r.Status().Update(ctx, cfg.KokuMetricsConfig); err != nil {
 			log.Error(err, "failed to update MetricsConfig status")
 		}
 		return ctrl.Result{}, err
@@ -699,7 +699,7 @@ func (r *MetricsConfigReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 		// obtain credentials token/basic & return if there are authentication credential errors
 		if err := setAuthentication(r, authConfig, cfg, req.NamespacedName); err != nil {
-			if err := r.Status().Update(ctx, cfg); err != nil {
+			if err := r.Status().Update(ctx, cfg.KokuMetricsConfig); err != nil {
 				log.Error(err, "failed to update MetricsConfig status")
 			}
 			return ctrl.Result{}, err
@@ -746,7 +746,7 @@ func (r *MetricsConfigReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 	}
 	cfg.Status.Packaging.PackagedFiles = uploadFiles
 
-	if err := r.Status().Update(ctx, cfg); err != nil {
+	if err := r.Status().Update(ctx, cfg.KokuMetricsConfig); err != nil {
 		log.Error(err, "failed to update MetricsConfig status")
 		result = ctrl.Result{}
 		errors = append(errors, err)
