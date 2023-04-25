3a9d244bf952d293285fbbf604cf467a201a9eda
maskarb <mskarbek@redhat.com>
2023-04-24 22:29:48 -0400

    update test

--- collector/collector_test.go
+++ collector/collector_test.go
@@ -356,7 +356,7 @@ func TestGenerateReportsNoNodeData(t *testing.T) {
 		},
 		TimeSeries: &fakeTimeRange,
 	}
-	if err := GenerateReports(fakeCR, fakeDirCfg, fakeCollector); err != nil {
+	if err := GenerateReports(fakeCR, fakeDirCfg, fakeCollector); err != nil && err != ErrNoData {
 		t.Errorf("Failed to generate reports: %v", err)
 	}
 	wanted := "No data to report for the hour queried."
