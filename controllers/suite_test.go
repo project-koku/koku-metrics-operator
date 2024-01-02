5a76550babb44a7d792114c52c85b87fde0971b8
maskarb <mskarbek@redhat.com>
2024-01-02 11:54:10 -0500

    use VolumeResourceRequirements instead of ResourceRequirements

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -144,7 +144,7 @@ var (
 		},
 		Spec: corev1.PersistentVolumeClaimSpec{
 			AccessModes: []corev1.PersistentVolumeAccessMode{"ReadWriteOnce"},
-			Resources: corev1.ResourceRequirements{
+			Resources: corev1.VolumeResourceRequirements{
 				Requests: corev1.ResourceList{
 					corev1.ResourceStorage: *resource.NewQuantity(10*1024*1024*1024, resource.BinarySI),
 				},
